471f7c454f3350577b41679725b5c832cb191953
stefa <stefa@DESKTOP-NJ0RTHC>
2019-03-29 21:25:22 +0100

    Punto 1

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,48 @@
 package it.polito.tdp.spellchecker.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class SpellCheckerController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextArea txtWord;
+
+    @FXML
+    private Button btnSpellChecker;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnClearText;
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnSpellChecker != null : "fx:id=\"btnSpellChecker\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+
+    }
 }
